835393b6a6f3e39591196be35a71cfa839f2af2c
simpleJs <97476942+smpljs@users.noreply.github.com>
2023-11-26 08:43:31 +0100

    Update appjs.js

--- appjs.js
+++ appjs.js
@@ -1,66 +1,87 @@
+
 // Simplejs Library Coded by Usman Alibaba
 
 // Writes the specified string to the document using document.write().
+
 function Print(str) {
     document.write(str);
 }
 
+// write the string to the document  
+
+function print(ptr){
+  document.write(ptr);
+
+}
+
 // Evaluates the provided string as JavaScript code and returns the result.
+
 function evl(ev) {
     return eval(ev);
 }
 
 // Displays an alert with the content passed as an argument.
+
 function flash(flh) {
     return alert(flh);
 }
 
 // Retrieves the DOM element with the specified id.
+
 function dom_id(id) {
     return document.getElementById(id);
 }
 
 // Retrieves an array of DOM elements with the specified name attribute.
+
 function dom_name(nm) {
     return document.getElementsByName(nm);
 }
 
 // Copies the provided text to the clipboard using the Clipboard API.
+
 function copy_text(cp) {
     return navigator.clipboard.writeText(cp);
 }
 
 // Converts the provided string to uppercase.
+
 function upper(ml) {
     return ml.toUpperCase();
 }
 
 // Converts the provided string to lowercase.
+
 function lower(lw) {
     return lw.toLowerCase();
 }
 
 // Retrieves the first DOM element that matches the specified CSS selector.
+
 function selector(sl) {
     return document.querySelector(sl);
 }
 
 // Retrieves a NodeList of all DOM elements that match the specified CSS selector.
+
 function selectAll(sll) {
     return document.querySelectorAll(sll);
 }
 
 // Retrieves a collection of DOM elements with the specified class name.
+
 function dom_class(clssssss) {
     return document.getElementsByClassName(clssssss);
 }
 
 // Returns the length of the provided string or array.
+
 function len(ln) {
     return ln.length;
 }
 
 // Navigates to the specified URL, effectively redirecting the page.
+
 function open(rdc) {
     return window.location.href = rdc;
 
@@ -87,15 +108,19 @@ function get_item(itm){
     
 }
 // Throw Erro message if Something went Wrong
+
+
 function error_msg(err = "Try again Something Went Wrong") {
   document.write(err);
 }
 
-function event(element, eventType, callback) {
+
+/*function event(element, eventType, callback) {
   element.addEventListener(eventType, callback);
-}
+}*/
 
 // remove event
+
 function remove(elem, eventType, callback) {
     elem.removeEventListener(eventType, callback);
 }
@@ -103,51 +128,123 @@ function remove(elem, eventType, callback) {
 
  
 // get langueg in HTML DOC
+
+
 function dom_lang(lng = "lang") {
   return document.documentElement.getAttribute(lng);
 
 }
 
+
+
+
 // Check if Network is Available
 
-function status(onln = navigator.onLine){
-return navigator.onLine;
+
+async function status() {
+  try {
+    await fetch('https://www.google.com', { method: 'HEAD' });
+    return true; // Online
+  } catch (error) {
+    return false; // Offline
+  }
 }
 
+// Event listener for online status changes
+
+function onOnline() {
+  console.log('Online');
+  
+  // Additional actions when online
+}
+
+// Event listener for offline status changes
+
+function onOffline() {
+  console.log('Offline');
+  
+  // Additional actions when offline
+}
+
+// Add event listeners to your library
+
+function addStatusListeners() {
+  window.addEventListener('online', onOnline);
+  window.addEventListener('offline', onOffline);
+}
+
+// Remove event listeners when necessary
+
+function removeStatusListeners() {
+  window.removeEventListener('online', onOnline);
+  window.removeEventListener('offline', onOffline);
+}
+
+
+
+
+
+
+
+
 // return user Platforn
+
 function platform(pltf = navigator.platform){
  return navigator.platform;
 }
 
 
 // Return user browser And platform
+
 function user_agent(brz = navigator.userAgent){
 return navigator.userAgent;
 }
+
+
 
 // return app version 
 
 function app_version(apvs = navigator.appVersion){
 return navigator.appVersion;
 }
+
+
 // return app code name
+
 function app_code(apcde = navigator.appCodeName){
  return navigator.appCodeName;
 }
+
+
 
 // return The languege of the app
 function app_lang(aplng = navigator.language){
  return navigator.language;
 }
+
+
 // return app name
+
 function app_name(apnme){
  return navigator.appName;
 }
+
+
+// A simple list like array
+
+function list(...args) {
+  return args;
+}
+
+
 
 // Change Html background color//
 function bg_color(bgcl){
 return document.body.style.backgroundColor = bgcl;
 }
+
+
+
 
 //  host ip Address Coded by Usman Alibaba
 
@@ -180,14 +277,12 @@ function host_ip(domain, callback) {
 }
 
 
-function erro_msg(msg) {
-return msg = "something";
 
-document.write(msg)
-
-}
 
 // Host IP Address function
+
+
+
 function source_code(domain, callback) {
     fetch("https://perfectionistic-own.000webhostapp.com/source_code.php", {
         method: "POST",
@@ -212,4 +307,25 @@ function source_code(domain, callback) {
             callback(error, null);
         }
     });
-} 
+}
+
+
+// item key
+function item_key(itk) {
+
+  return window.localStorage.key(itk);
+}
+
+
+
+// remove item in local storage
+function remove_item(rmvi){
+return window.localStorage.removeItem(rmvi);
+}
+
+// Clear All data in local storage
+
+function clear_item(cli = localStorage.clear(){
+
+return window.localStorage.clear();
+}
